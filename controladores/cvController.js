ae21be84def49f1579da8272ff11b5def539b660
nestor <jokera666@gmail.com>
2019-11-06 01:21:26 +0100

    Actualizacion del CV con la nueva experiencia y algunos retoques de estilos.

--- controladores/cvController.js
+++ controladores/cvController.js
@@ -11,47 +11,65 @@ angular.module('appCV.contrsCV', [])
     skype:     'dokich',
     github0:    'https://github.com/nestoredrev',
     github1:    'https://github.com/jokera666',
-    linkedin:  'https://www.linkedin.com/in/nestor-dobrinov-edrev/'
-
+    linkedin:  'https://www.linkedin.com/in/nestor-dobrinov-edrev/',
+    steam:     'https://steamcommunity.com/profiles/76561198054601184/'
   };
 
   var POSITION_TRANSLATED0 = $translate.instant('POSITION_DESCRIPTION0');
   var POSITION_TRANSLATED1 = $translate.instant('POSITION_DESCRIPTION1');
   var POSITION_TRANSLATED2 = $translate.instant('POSITION_DESCRIPTION2');
   var POSITION_TRANSLATED3 = $translate.instant('POSITION_DESCRIPTION3');
+  var POSITION_TRANSLATED4 = $translate.instant('POSITION_DESCRIPTION4');
+  var POSITION_TRANSLATED5 = $translate.instant('POSITION_DESCRIPTION5');
 
   var PLACE_TRANSLATED0 = $translate.instant('PLACE_DESCRIPTION0');
   var PLACE_TRANSLATED1 = $translate.instant('PLACE_DESCRIPTION1');
   var PLACE_TRANSLATED2 = $translate.instant('PLACE_DESCRIPTION2');
   var PLACE_TRANSLATED3 = $translate.instant('PLACE_DESCRIPTION3');
+  var PLACE_TRANSLATED4 = $translate.instant('PLACE_DESCRIPTION4');
+  var PLACE_TRANSLATED5 = $translate.instant('PLACE_DESCRIPTION5');
 
   var PERIOD_TRANSLATED0 = $translate.instant('PERIOD_DESCRIPTION0');
   var PERIOD_TRANSLATED1 = $translate.instant('PERIOD_DESCRIPTION1');
   var PERIOD_TRANSLATED2 = $translate.instant('PERIOD_DESCRIPTION2');
   var PERIOD_TRANSLATED3 = $translate.instant('PERIOD_DESCRIPTION3');
+  var PERIOD_TRANSLATED4 = $translate.instant('PERIOD_DESCRIPTION4');
+  var PERIOD_TRANSLATED5 = $translate.instant('PERIOD_DESCRIPTION5');
 
   $scope.experiencia = [
     {
       oficio:  POSITION_TRANSLATED0,
       lugar:   PLACE_TRANSLATED0,
       periodo: PERIOD_TRANSLATED0,
-      lugarUrl: 'http://scorpora.eu/'
+      lugarUrl: 'https://visualtis.com/'
     },
     {
       oficio:  POSITION_TRANSLATED1,
       lugar:   PLACE_TRANSLATED1,
       periodo: PERIOD_TRANSLATED1,
-      lugarUrl: 'http://umh.es/'
+      lugarUrl: 'https://www.stratebi.com/'
     },
     {
       oficio:  POSITION_TRANSLATED2,
       lugar:   PLACE_TRANSLATED2,
-      periodo: PERIOD_TRANSLATED2
+      periodo: PERIOD_TRANSLATED2,
+      lugarUrl: 'http://scorpora.eu/'
     },
     {
       oficio:  POSITION_TRANSLATED3,
       lugar:   PLACE_TRANSLATED3,
       periodo: PERIOD_TRANSLATED3,
+      lugarUrl: 'http://umh.es/'
+    },
+    {
+      oficio:  POSITION_TRANSLATED4,
+      lugar:   PLACE_TRANSLATED4,
+      periodo: PERIOD_TRANSLATED4
+    },
+    {
+      oficio:  POSITION_TRANSLATED5,
+      lugar:   PLACE_TRANSLATED5,
+      periodo: PERIOD_TRANSLATED5,
       lugarUrl: 'http://www.airenetworks.es/' 
     }
   ];
@@ -111,12 +129,14 @@ angular.module('appCV.contrsCV', [])
     {tec:'MySQL'},
     {tec:'JSON'},
     {tec:'C,C++'},
-    {tec:'Git'}
+    {tec:'Git'},
+    {tec:'Pentaho CDE'}
   ];
 
   $scope.frames = [
     {frame: 'Ionic'},
-    {frame: 'AngularJS'},
+    {frame: 'Angular'},
+    {frame: 'Nodejs'},
     {frame: 'jQuery'},
     {frame: 'Bootstrap'},
     {frame: 'Materialize'}
